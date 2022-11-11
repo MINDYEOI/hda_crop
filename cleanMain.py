717d6f37844995cb236d9b449d3c4c1df620a797
MINDYEOI <mindyeoi@khu.ac.kr>
2022-11-11 17:27:13 +0900

    [fix] Add break

--- cleanMain.py
+++ cleanMain.py
@@ -29,3 +29,5 @@ for camNo in camNos:
                         cropImg, label, subDetList[0], subDetList[1])
                     jebal.saveAllClean(
                         cropImg, label, subDetList[0], subDetList[1])
+
+                    break
